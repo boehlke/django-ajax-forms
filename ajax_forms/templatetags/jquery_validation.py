68fd347f7adabde6d3de64af1a8aed2881356112
André Böhlke <a.boehlke@gmail.com>
2013-07-18 13:55:18 +0200

    rename media to status

--- ajax_forms/templatetags/jquery_validation.py
+++ ajax_forms/templatetags/jquery_validation.py
@@ -3,14 +3,10 @@ import os
 from django import template
 
 import ajax_forms
+from django.conf import settings
 
 register = template.Library()
 
-VALIDATION_SCRIPT = None
-
 def include_validation():
-    global VALIDATION_SCRIPT
-    if VALIDATION_SCRIPT is None:
-        VALIDATION_SCRIPT = open(os.path.join(os.path.dirname(ajax_forms.__file__), 'media', 'ajax_forms', 'js', 'jquery-ajax-validation.js')).read()
-    return '''<script type="text/javascript">%s</script>''' % VALIDATION_SCRIPT
+    return '''<script type="text/javascript" src="%sajax_forms/js/jquery-ajax-validation.js"></script>''' % settings.STATIC_URL
 register.simple_tag(include_validation)
